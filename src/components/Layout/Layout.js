40e16529000f6102cda2197288c0b6f86d205518
Mason Jennings <mminute@users.noreply.github.com>
2020-09-07 18:29:30 -0700

    Build the contact page

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,14 +5,26 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect, useRef, useState } from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import Header from '../Header/Header';
 import Footer from '../Footer';
 import './Layout.css';
 
-const Layout = ({ children, headerLinks, stickyHeader }) => {
+const Layout = ({ children, headerLinks, stickyHeader, stickyFooter }) => {
+  const contentRef = useRef();
+  const footerRef = useRef();
+  const [fixedFooter, setFixedFooter] = useState(false);
+
+  useEffect(() => {
+    if (stickyFooter && contentRef.current && footerRef.current) {
+      const contentHeight = contentRef.current.offsetParent.clientHeight;
+      if (window.innerHeight > contentHeight + footerRef.current.clientHeight) {
+        setFixedFooter(true);
+      }
+    }
+  });
+
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -30,16 +42,16 @@ const Layout = ({ children, headerLinks, stickyHeader }) => {
         siteTitle={data.site.siteMetadata.title}
         stickyHeader={stickyHeader}
       />
-      <main>
-        {children}
-      </main>
-      <Footer />
+      <main ref={contentRef}>{children}</main>
+
+      <div
+        ref={footerRef}
+        style={fixedFooter ? { position: 'fixed', bottom: 0, left: 0, width: '100%' } : {}}
+      >
+        <Footer />
+      </div>
     </>
   );
-};
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 };
 
 export default Layout;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,12 +2,94 @@ import React from 'react';
 import Layout from '../components/Layout/Layout';
 import SEO from '../components/seo';
 import navLinks from '../components/DATA/navLinks';
+import { Box } from 'gestalt';
 
 function Contact() {
   return (
-    <Layout headerLinks={[navLinks.home]}>
+    <Layout headerLinks={[navLinks.home]} stickyHeader stickyFooter>
       <SEO title="Contact" />
-      <h1>Contact page</h1>
+      <div className="container">
+        <Box marginTop={12} padding={6}>
+          <Box display="flex" justifyContent="center">
+            <h2 className="montserrat" style={{ fontSize: '30px' }}>GET IN TOUCH</h2>
+          </Box>
+
+          <Box display="flex" justifyContent="center" marginTop={12}>
+            <div className="inconsolata gray" style={{ fontSize: '12px' }}>
+              I'D LOVE TO HEAR FROM YOU! I'LL GET BACK TO YOU SHORTLY.
+            </div>
+          </Box>
+        </Box>
+
+        <div style={{ marginBottom: '50px', marginTop: '50px' }}>
+          <form action="//formspree.io/masonjennings.dev@gmail.com" method="POST">
+            <Box display="flex" justifyContent="center">
+              <Box display="flex" direction="column" column={6}>
+                <Box paddingY={2}>
+                  <input
+                    aria-invalid="false"
+                    className="form-control"
+                    data-validation-required-message="Please enter your name."
+                    name="name"
+                    placeholder="Name"
+                    required=""
+                    type="text"
+                    style={{ width: '100%' }}
+                  />
+                </Box>
+
+                <Box paddingY={2}>
+                  <input
+                    className="form-control"
+                    data-validation-required-message="Please enter your email address."
+                    name="_replyto"
+                    placeholder="Your E-mail"
+                    required=""
+                    type="email"
+                    style={{ width: '100%' }}
+                  />
+                </Box>
+
+                <Box paddingY={2}>
+                  <textarea
+                    className="form-control"
+                    data-validation-required-message="Please enter your message."
+                    name="message"
+                    placeholder="Message"
+                    required=""
+                    rows="7"
+                    style={{ width: '100%' }}
+                  />
+                </Box>
+
+                <Box paddingY={2}>
+                  <input
+                    type="submit"
+                    value="Send"
+                    className="btn btn-round btn-g"
+                  />
+                </Box>
+                
+                <input
+                  type="hidden"
+                  name="_next"
+                  value="//masonjennings.io/thanks.html"
+                />
+                <input
+                  type="hidden"
+                  name="_subject"
+                  value="New contact @masonjennings.io"
+                />
+                <input
+                  type="text"
+                  name="_gotcha"
+                  style={{ display: 'none' }}
+                />
+              </Box>
+            </Box>
+          </form>
+        </div>
+      </div>
     </Layout>
   );
 }
